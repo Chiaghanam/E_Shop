b69c781b17ee2c08b753590d4a8fa57e75624bdc
Amobi <leofazzy98@gmail.com>
2026-02-26 09:32:29 +0100

    Fix double api prefix on token URLs

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -12,8 +12,8 @@ urlpatterns = [
     path('product/edit/<int:pk>/', views.product_edit, name='product_edit'),
     path('productDetail/<int:pk>/', views.product_detail_view, name='product_detail'),
     
-    path('api/token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     
     path('users/', Get_User, name='users'),
     path('users/profile/', Get_User_Profile, name='user-profile'),
