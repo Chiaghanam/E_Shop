7bd1c9447c83988edf0d3d0d3b1cdd67de0771b9
Amobi <leofazzy98@gmail.com>
2026-02-26 09:02:57 +0100

    Update API URL and ALLOWED_HOSTS for production

--- backend/front/src/component/Constants/api.js
+++ backend/front/src/component/Constants/api.js
@@ -1,6 +1,2 @@
-export const PROXY = 'http://127.0.0.1:8000/api';
-
-// Update MEDIA_URL to avoid duplicate /media/ in the URL
-// export const MEDIA_URL = '';
-
+export const PROXY = 'https://e-shop-cl34.onrender.com/api';
 const paypal_key = 'AS_v5h8RtJTU22baTF4bl8RpnLdYK6Gr4Lz_eaCNzSpP4lbsPy69_vNu2vvg5dqFBViB3Z7ydZCFphUO';
